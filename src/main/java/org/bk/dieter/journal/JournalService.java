828e54cb5722da18c802b48d8afcffb63f199b4f
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-09-02 01:12:51 +0200

    journal service added

---
+++ src/main/java/org/bk/dieter/journal/JournalService.java
@@ -0,0 +1,33 @@
+package org.bk.dieter.journal;
+
+import org.bk.dieter.product.Product;
+import org.bk.dieter.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+/**
+ * Created by redi on 2016-08-30.
+ */
+@Service
+@Transactional(propagation = Propagation.REQUIRED)
+public class JournalService {
+
+    @Autowired
+    JournalRepository journalRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    public Journal addProductToTheJournal(Journal journal, Product product) {
+        Optional<Product> productOptional = productRepository.findById(product.getId());
+        if (!productOptional.isPresent()) {
+            productRepository.save(product);
+        }
+        journal.getProductList().add(product);
+        return journal;
+    }
+}
