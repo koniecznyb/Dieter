61b8dcfe3b005a48c16f1b8faadc43b1c41529e2
Bartłomiej Konieczny <bartlomiej.konieczny@sabre.com>
2016-09-02 10:32:03 +0200

    role entity implementation

---
+++ src/main/java/org/bk/dieter/role/Role.java
@@ -0,0 +1,31 @@
+package org.bk.dieter.role;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bk.dieter.user.Customer;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Bartłomiej Konieczny on 02-Sep-16.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_id_gen")
+    @SequenceGenerator(name = "role_id_gen", sequenceName = "seq_role_id", allocationSize = 1, initialValue = 1)
+    private Long id;
+
+    @Column(name = "role_name", nullable = false)
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<Customer> customers = new HashSet<>();
+}
