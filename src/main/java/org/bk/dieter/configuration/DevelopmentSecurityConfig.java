7e63cde43fc4d5c9cf54fb9f2b1ad3421908671e
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-09-04 00:21:20 +0200

    changed to external postgresql database

--- src/main/java/org/bk/dieter/configuration/DevelopmentSecurityConfig.java
+++ src/main/java/org/bk/dieter/configuration/DevelopmentSecurityConfig.java
@@ -1,24 +1,33 @@
 package org.bk.dieter.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * Created by redi on 05.04.2016.
- */
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final String URL = "jdbc:postgresql://localhost:5432/dieter";
+    private final String USER = "dieter";
+    private final String PASSWORD = "q1w2e3r4";
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        return new DriverManagerDataSource(URL, USER, PASSWORD);
     }
 
 }
