1d1f86ed771fddc23c73de5e4645729bf4f95293
Redi <konieczny93@gmail.com>
2016-09-06 00:06:09 +0200

    updated env config

--- src/main/java/org/bk/dieter/configuration/DevelopmentConfiguration.java
+++ src/main/java/org/bk/dieter/configuration/DevelopmentConfiguration.java
@@ -1,8 +1,12 @@
 package org.bk.dieter.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by redi on 14.05.2016.
@@ -11,4 +15,22 @@ import org.springframework.context.annotation.Profile;
 @Profile("development")
 public class DevelopmentConfiguration {
 
+    private final String URL = "jdbc:postgresql://localhost:5432/dieter_database";
+    private final String USER = "dieter_user";
+    private final String PASSWORD = "q1w2e3r4";
+
+    @Bean
+    public DataSource getDataSource() {
+        return new DriverManagerDataSource(URL, USER, PASSWORD);
+    }
+
+    @Bean
+    public Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
+        properties.put("hibernate.hbm2ddl.auto", "create");
+        properties.put("hibernate.show_sql", "true");
+        return properties;
+    }
 }
