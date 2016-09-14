04dff434450bbdad8c9822ad1700481b452cc5d9
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-09-14 22:40:51 +0200

    updated config

--- src/main/java/org/bk/dieter/configuration/development/DevelopmentConfiguration.java
+++ src/main/java/org/bk/dieter/configuration/development/DevelopmentConfiguration.java
@@ -14,9 +14,9 @@ import javax.sql.DataSource;
 @Profile("development")
 public class DevelopmentConfiguration {
 
-    private final String URL = "jdbc:postgresql://localhost:5432/dieter_database";
-    private final String USER = "dieter_user";
-    private final String PASSWORD = "q1w2e3r4";
+    private final String URL = "jdbc:postgresql://localhost:5432/dieter";
+    private final String USER = "dieter";
+    private final String PASSWORD = "q1w2e3r4"
 
     @Bean
     public DataSource getDataSource() {
