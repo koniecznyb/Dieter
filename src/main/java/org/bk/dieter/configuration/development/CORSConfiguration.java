a69bd7750da90fbaf7c1695dbe2947e38aa3d5cd
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-11-26 21:49:46 +0100

    added cors to the api

---
+++ src/main/java/org/bk/dieter/configuration/development/CORSConfiguration.java
@@ -0,0 +1,18 @@
+package org.bk.dieter.configuration.development;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by SG0224958 on 26.11.16.
+ */
+@Configuration
+public class CORSConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
