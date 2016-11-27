c974f1b3b02c627bea482437c6228e8fa7e6d1db
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-11-27 23:50:55 +0100

    improve cors configuration, improve date format in api

--- src/main/java/org/bk/dieter/configuration/development/CORSConfiguration.java
+++ src/main/java/org/bk/dieter/configuration/development/CORSConfiguration.java
@@ -12,7 +12,8 @@ public class CORSConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
     }
 
 }
