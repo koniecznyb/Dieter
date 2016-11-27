1ac64bf07d631fef7a4b9063815eeba7febea61f
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-11-27 12:16:58 +0100

    do not require authentication for Options headers (cors)

--- src/main/java/org/bk/dieter/configuration/development/DevelopmentSecurityConfig.java
+++ src/main/java/org/bk/dieter/configuration/development/DevelopmentSecurityConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,11 +25,13 @@ public class DevelopmentSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         LOG.info("Configuring security filter");
-        http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().fullyAuthenticated();
-        http.httpBasic();
-        http.csrf().disable();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()//allow CORS option calls
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 
 
