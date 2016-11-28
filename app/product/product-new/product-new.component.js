dd5fe46d7d0a11c9c176b0fd521c89b28f1a9bf2
Bartłomiej Konieczny <konieczny93@gmail.com>
2016-11-28 14:28:28 +0100

    new product initial

---
+++ app/product/product-new/product-new.component.js
@@ -0,0 +1,8 @@
+"use strict";
+var NewProductComponent = (function () {
+    function NewProductComponent() {
+    }
+    return NewProductComponent;
+}());
+exports.NewProductComponent = NewProductComponent;
+//# sourceMappingURL=product-new.component.js.map
